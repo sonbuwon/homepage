6ae02e12597c718b6c6fa42e348cd2226d84f9e7
sonbuwon <bwgh0268@naver.com>
2024-02-27 17:47:54 +0900

    spring boot project for aws ec2

---
+++ src/main/java/com/example/awsone/AwsoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awsone/AwsoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
